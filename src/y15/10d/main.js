e243002d78e4245b9a4291398ef1d6474e97215f
luki333 <134687577+luki333@users.noreply.github.com>
2023-07-02 23:08:23 +0200

    Add day 10

---
+++ src/y15/10d/main.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env bun
+import input from './input.txt';
+
+let str1 = input;
+for (let i = 0; i < 40; i++) {
+  str1 = lookAndSay(str1);
+}
+
+let str2 = str1;
+for (let i = 0; i < 10; i++) {
+  str2 = lookAndSay(str2);
+}
+
+console.log(str1.length, str2.length);
+
+function lookAndSay(str) {
+  const reRepeatedDight = new RegExp(/([0-9])\1*/g);
+  let result = '';
+  for (let match; (match = reRepeatedDight.exec(str)) !== null; )
+    result += match.at(0).length + match.at(1);
+  return result;
+}
